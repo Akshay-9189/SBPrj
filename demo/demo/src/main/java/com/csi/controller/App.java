93f9331642d6445d169d5e62279f2bc82e73d8f0
Akshay-9189 <akshaykumbhar957@gmail.com>
2022-07-26 12:43:40 +0530

    Initial Commit

---
+++ demo/demo/src/main/java/com/csi/controller/App.java
@@ -0,0 +1,15 @@
+package com.csi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class App {
+
+    @GetMapping("/a")
+    public String sayHello(){
+        return "Welcome";
+    }
+}
